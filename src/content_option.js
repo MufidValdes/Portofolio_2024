0a979fcedf2851b4191251fd4e1ae9a8aff660d2
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-07-02 04:04:17 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,237 @@
+const logotext = "Mufid";
+const meta = {
+    title: "Muhamad Mufid Bachri",
+    description: "I’m Muhamad Mufid Bachri Android Developer _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Muhamad Mufid Bachri",
+    animated: {
+        first: "I develop mobile apps",
+        second: "I Full Stack Web Developer",
+        third: "I love coding ",
+    },
+    description: "I am a fresh graduate of Informatics Engineering who is passionate and ready to start a professional career. With a strong understanding of programming and software development. I have good communication skills and am ready to learn quickly. I look forward to joining your team to learn and grow together.",
+    your_img_url: "https://github.com/terminallystudio/ss_test/assets/174089516/3ef7593b-be92-4cd6-b4c2-4bfc621a461c",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I am a fresh graduate of Informatics Engineering who is passionate and ready to start a professional career. With a strong understanding of programming and software development, I was actively involved in projects during my studies. I have good communication skills and am ready to learn quickly. I look forward to joining your team to learn and grow together. Thank you for your attention.",
+};
+const education = [{
+        edutitle: "Senior High School",
+        position:"Natural Sciences",
+        value:"",
+        where: "Comal",
+        date: "2016 - 2019",
+    },
+    {
+        edutitle: "Al-qur'an University of Science",
+        position:" Bachelor of Informatics Engineering",
+        value:"  GPA 3.65",
+        where: "Wonosobo",
+        date: "2019 - 2023",
+    },
+];
+const worktimeline = [
+    {
+        jobtitle: "Project-Based Virtual Intern : Fullstack Developer BTPN Syariah x Rakamin Academy",
+        where: " BTPN Syariah · Magang",
+        date: " Apr 2024 - Mei 2024",
+        desc: [{
+            data:"During my internship at BTPN x Rakamin, I had the opportunity to deepen my understanding and skills in various technologies and programming languages.Some of the things I learned include:Understanding the basics and advanced concepts of Java."
+        },
+        {
+            data:"Mastering the basics of Golang programming."
+        }
+
+        ]
+    },
+    {
+        jobtitle: "Team Leader & Developer - Metaverse 3D Creation + Commercialization",
+        where: " Corporate Innovation Asia - CIAS · Magang",
+        date: " Agu 2022-Des 2022",
+        desc: [{
+            data:"Led the team to develop the metaverse on the Roblox Studio platform using the LUA Programming Language"
+        },
+        {
+            data:"Create project management using Agile methods and ensure projects are completed within the specified time."
+        }
+
+        ]
+    },
+    {
+        jobtitle: "Android Developer Cohort",
+        where: " Bangkit Academy led by Google, Tokopedia, Gojek, & Traveloka · Magang",
+        date: " Feb 2022-Jul 2022",
+        desc: [{
+            data:"Received a comprehensive education in Android app development."
+        },
+        {
+            data:"Actively contributed in designing, developing, and implementing application features."
+        },
+        {
+            data:"Participated in trainings that emphasize soft skill development such as self-branding, problem solving, design thinking, and collaboration."
+        }
+    
+        ]
+    },
+    {
+        jobtitle: "Lead Game Programer",
+        where: " DISCOMVISION- university Sains Al-Qur'an, Wonosobo",
+        date: "Feb 2022-Jul 2022",
+        desc: [{
+    
+            data:"Team Coordination: Ensuring all members of the game development team work efficiently and conform to the specified schedule."
+        },
+        {
+            data:"Development: Contribute to the development of thegame in the implementation of agreed features with the team."
+    
+        }
+    
+        ]
+    },
+];
+
+const skills = [{
+        name: "Html ,Css , Javascript",
+        value: 80,
+    },
+    {
+        name: "Kotlin",
+        value: 80,
+    },
+    {
+        name: "FLutter",
+        value: 50,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "NextJs",
+        value: 60,
+    },
+    {
+        name: "Vue",
+        value: 50,
+    },
+    {
+        name: "Golang",
+        value: 60,
+    },
+    {
+        name: "Laravel",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "",
+    },
+    {
+        title: "Mobile Apps",
+        description: "",
+    },
+    {
+        title: "web Design & Developer",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://github.com/MufidValdes/web-pemula/assets/81525382/bdf6c80e-dd0c-4e25-adb3-fa0bfd333382",
+        description: "HTML + CSS",
+        link: "https://github.com/MufidValdes/web-pemula",
+    },
+    {
+        img: "https://github.com/MufidValdes/Books-app-back-end/assets/81525382/71494513-11d7-49a2-88cd-3211d7e77995",
+        description: "NodeJs ",
+        link: "https://github.com/MufidValdes/Books-app-back-end",
+    },
+    {
+        img: "https://github.com/MufidValdes/bookshelf-apps/assets/81525382/75a11d4c-1fcd-4929-bcba-75c556dbe0a2",
+        description: "HTML + CSS + JS",
+        link: "https://github.com/MufidValdes/bookshelf-apps",
+    },
+    {
+        img: "https://github.com/MufidValdes/dashboard-job-hunt-nextjs/assets/81525382/416e9b43-4d0d-4f9f-9bb1-1edb7fa2dca1",
+        description: "NEXTJS + TAILWIND + TS + RAILWAY + POSTGRE + PRISMA",
+        link: "https://github.com/MufidValdes/dashboard-job-hunt-nextjs",
+    },
+    {
+        img: "",
+        description: "KOTLIN",
+        link: "https://github.com/MufidValdes/adv_base-githubApp",
+    },
+    {
+        img: "",
+        description: "KOTLIN.",
+        link: "https://github.com/MufidValdes/adv_base-iStory",
+    },
+
+    {
+        img: "https://github.com/MufidValdes/website-job-hunt-nextjs/assets/81525382/df7e3f31-4da3-4d60-ba9b-b7def1e982c2",
+        description: "NEXTJS + TAILWIND + TS + RAILWAY + POSTGRE + PRISMA",
+        link: "https://github.com/MufidValdes/website-job-hunt-nextjs",
+    },
+    {
+        img: "",
+        description: "lARAVEL-LIVEWARE + MYSQL",
+        link: "https://github.com/MufidValdes/laravel-liveware-test-crud",
+    },
+    {
+        img: "https://www.bhsystems.com.au/uploads/2/2/1/7/22173198/1404274400.jpg",
+        description: "GO + GIN + JWT + MYSQL",
+        link: "#",
+    },
+    {
+        img: "https://www.bhsystems.com.au/uploads/2/2/1/7/22173198/1404274400.jpg",
+        description: "FLUTTER",
+        link: "#",
+    },
+    {
+        img: "https://www.bhsystems.com.au/uploads/2/2/1/7/22173198/1404274400.jpg",
+        description: "GO + GIN + JWT + MYSQL ",
+        link: "#",
+    },
+    {
+        img: "https://www.bhsystems.com.au/uploads/2/2/1/7/22173198/1404274400.jpg",
+        description: "VUE + NUXT ",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "muhamadmufidbachri27@gmail.com",
+    YOUR_FONE: "(+62)820-0633-9191",
+    description: "Feel free to reach out to me through any of the following contact methods. I am available for freelance work, collaborations, or any inquiries you may have. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_@!qwerty",
+    YOUR_TEMPLATE_ID: "template_@!Qwerty",
+    YOUR_USER_ID: "VouAn_vTvTjhiEOcY",
+};
+
+const socialprofils = {
+    github: "https://github.com/MufidValdes",
+    facebook: "https://www.facebook.com/muhamad.m.bachri/",
+    linkedin: "https://www.linkedin.com/in/muhamad-mufid-bachri-455197219/",
+    twitter: "",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    education,
+};
